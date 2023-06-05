0868c98c17637d04717be232855c2012160de60d
fedetarable <118947629+fedetarable@users.noreply.github.com>
2023-06-05 09:56:23 -0300

    Add files via upload

---
+++ Assets/JS/upcomingEvents.js
@@ -0,0 +1,44 @@
+const currentDate = new Date(data.currentDate);
+
+const events = data.events;
+
+let upcomingEventsArr = [];
+
+function upcomingEvents(events, currentDate) {
+    return events.filter((event) => {
+        const eventDate = new Date(event.date);
+        if (eventDate > currentDate) {
+            upcomingEventsArr.push(event);
+        }
+        return upcomingEventsArr;
+    });
+}
+
+upcomingEvents(events, currentDate)
+
+console.log("🚀 ~ file: home.js:22 ~ upcomingEventsArr:", upcomingEventsArr[0].image)
+
+let htmlUpcoming = "";
+
+for (let i = 0; i < upcomingEventsArr.length; i++) {
+    htmlUpcoming += `
+    
+    <div class="card mx-3 mt-3" style="width: 18rem;">
+                <img src="${upcomingEventsArr[i].image}" class="cardImgStandard card-img-top" alt="...">
+                <div class="card-body d-flex flex-column align-items-center">
+                    <h5 class="card-title">${upcomingEventsArr[i].name}</h5>
+                    <p class="card-text">${upcomingEventsArr[i].description}</p>
+                    <div class="d-flex justify-content-between align-items-baseline w-100">
+                        <p>${upcomingEventsArr[i].price}</p>
+                        <a href="http://127.0.0.1:5500/Assets/detail.html" class="btn btn-danger">Details</a>
+                    </div>
+                </div>
+            </div>
+
+
+    `;
+}
+
+const upcomingEventsContainer = document.querySelector(".upcomingEventsContainer");
+
+upcomingEventsContainer.innerHTML = htmlUpcoming;
